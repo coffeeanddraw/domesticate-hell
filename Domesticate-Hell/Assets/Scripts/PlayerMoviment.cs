e009a41d76022e75c2991d733a72e38ef7fc141b
AngeloCM <angelocasagrande@live.com>
2019-09-12 22:52:54 -0500

    New Scripts
    - PlayerMoviment Script added
    - Some sprites to test level

---
+++ Domesticate-Hell/Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    [SerializeField]float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    Rigidbody2D playerRB;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(x * runSpeed, playerRB.velocity.y, 0f);
+        playerRB.velocity = move;
+    }
+
+}
