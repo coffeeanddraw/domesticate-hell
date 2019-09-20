c8695410bb2900f7d21383cf665e18bfdb2685fc
Angelo de Mello <Angelo.deMello@iam.local>
2019-09-20 18:11:52 -0500

    Going to the Gym !
    - Player_Interaction
    - Interactive_Object

---
+++ Domesticate-Hell/Assets/Scripts/Interactive_Object.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive_Object : MonoBehaviour
+{
+    public void doInteraction()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Domesticate-Hell/Assets/Scripts/Player Scripts/Player_Interaction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Interaction : MonoBehaviour
+{
+    [SerializeField] private GameObject currentObject;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") && currentObject)
+        {
+            currentObject.SendMessage("doInteraction");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            Debug.Log("other.name");
+            currentObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            currentObject = null;
+        }
+    }
+
+}
