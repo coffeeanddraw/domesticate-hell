6a982c16e8a6162471d4e2c8db564f74de7fffc8
Venelope <threeeyedvenelope@gmail.com>
2019-09-30 15:06:18 -0700

    Create PetMovement script
    PetMovement.cs programmatically moves the pet back and forth

---
+++ Domesticate-Hell/Assets/Scripts/Pet Scripts/PetMovement.cs
@@ -0,0 +1,54 @@
+﻿///////////////////////////////////
+///// ⚔🦂🐾BAT GIRL🐾🦂⚔ /////
+///////////////////////////////////
+//////DOMESTICATE HELL - 2019//////
+///////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetMovement : MonoBehaviour
+{
+    Rigidbody2D rgbd2D;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float changeTime = 3.0f;
+
+    private float timer;
+    private int direction = 1;
+    private bool collision = false;
+
+    void Start()
+    {
+        rgbd2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Idle();
+    }
+
+    // * Run Finite State Machine *// 
+    private void Idle()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0 || collision)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+
+        Vector2 position = rgbd2D.position;
+
+        position.x = position.x + Time.deltaTime * speed;
+
+        rgbd2D.MovePosition(position);
+    }
+}
