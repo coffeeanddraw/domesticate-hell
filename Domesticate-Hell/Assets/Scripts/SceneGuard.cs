91909f91253d8b5b0c254e935bdd8d144ed569d0
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2019-09-30 06:38:33 -0500

    Add SceneGuard script
    SceneGuard manages scene changes

---
+++ Domesticate-Hell/Assets/Scripts/SceneGuard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneGuard : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName = "";
+   
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
