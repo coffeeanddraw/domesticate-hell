4eb8f4b3cff2d1cdd4de97da4c95e2411486dd37
Venelope <threeeyedvenelope@gmail.com>
2019-10-15 04:20:40 -0500

    Fix Dialogue Canvas

--- Domesticate-Hell/Assets/Scripts/Other/HellishTime.cs
+++ Domesticate-Hell/Assets/Scripts/Other/HellishTime.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class HellishTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Text clockText;
+
+    [SerializeField]
+    private Text dayText;
+
+    private const float secondsPerDay = 120f;
+
+    private int days = 0;
+    private int hours = 20;
+    private int mintues = 0;
+    private int seconds = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        while(true)
+        {
+            seconds += (int)Math.Round(Time.deltaTime);
+            clockText.text = seconds.ToString();
+        }
     }
 }
