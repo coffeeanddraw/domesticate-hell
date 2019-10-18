272703aab02e3b8d9049b1eb4856ef83ffd078ef
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-10-18 15:22:09 -0400

    Transferred UI-Ishan to the updated branch for master

---
+++ Domesticate-Hell/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool PausedEvent = false;
+
+    public GameObject PausedUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PausedEvent)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PausedUI.SetActive(false);
+        Time.timeScale = 1f;
+        PausedEvent = false;
+    }
+
+    void Paused()
+    {
+        PausedUI.SetActive(true);
+        Time.timeScale = 0f;
+        PausedEvent = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game....");
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Options");
+    }
+}
