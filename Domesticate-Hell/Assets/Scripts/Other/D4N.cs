c58a806553eddd6f2fa54a2b04dbc37b3375cc73
Cattatonicat <threeeyedvenelope@gmail.com>
2019-10-26 04:13:45 -0500

    Add D4N script

---
+++ Domesticate-Hell/Assets/Scripts/Other/D4N.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D4N : MonoBehaviour
+{
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player is with D4N");
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player is saying bye to D4N");
+        }
+    }
+}
