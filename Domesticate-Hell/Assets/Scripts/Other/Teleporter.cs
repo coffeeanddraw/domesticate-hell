532d069a5291286a23d924572630db064789ac25
Cattatonicat <threeeyedvenelope@gmail.com>
2019-10-28 04:16:30 -0500

    Update environment (#119)

---
+++ Domesticate-Hell/Assets/Scripts/Other/Teleporter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject destinationTeleporter;
+
+    [SerializeField]
+    private GameObject magenta;
+
+    private Transform destinationLocation;
+    private Transform magentaLocation;
+
+    void Awake()
+    {
+        destinationLocation = destinationTeleporter.GetComponent<Transform>();
+        magentaLocation = magenta.GetComponent<Transform>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
