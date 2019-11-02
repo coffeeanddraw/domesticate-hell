760c3d5bba81f1ba108c195578a78e003eddfc52
Cattatonicat <threeeyedvenelope@gmail.com>
2019-11-02 08:17:08 -0500

    Fix masters branch

--- Domesticate-Hell/Assets/Scripts/Other/PauseMenu.cs
+++ Domesticate-Hell/Assets/Scripts/Other/PauseMenu.cs
@@ -9,6 +9,18 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static bool PausedEvent = false;
+
+    [SerializeField]
+    private GameObject PausedUI;
+
+
+    void Awake()
+    {
+        PausedUI.SetActive(false);
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
